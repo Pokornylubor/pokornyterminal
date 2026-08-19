bf87b2a93c5cc637b77ee3d5c7074df353797ba2
Pokornylubor <pokornyl98@gmail.com>
2026-08-19 21:55:22 +0200

    Update START.bat

--- START.bat
+++ START.bat
@@ -1,25 +1,5 @@
 @echo off
-cls
 echo ==================================================
-echo              POKORNY TERMINAL - START
+echo         SPOUSTIM POKORNY TERMINAL...
 echo ==================================================
-echo.
-echo [1] Rychly start (Spusti aplikaci ihned)
-echo [2] Aktualizace (Stahne nova data - trva cca 15 minut)
-echo.
-set /p volba="Vyber moznost (zadej 1 nebo 2 a stiskni Enter): "
-
-if "%volba%"=="2" (
-    echo.
-    echo --------------------------------------------------
-    echo SPOUSTIM ROBOTA NA STAHOVANI DAT...
-    echo (Proces potrva cca 10-15 minut)
-    echo --------------------------------------------------
-    python updater.py
-    echo.
-    echo Data uspesne stazena!
-)
-
-echo.
-echo Zapinam webove rozhrani Streamlit...
-streamlit run Dashboard.py
+streamlit run 1_Home.py --server.address=127.0.0.1
