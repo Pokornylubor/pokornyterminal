55e4358105785227638d2b39cf7afcf7e57f6aa5
Lubor Pokorny <pokornyl98@gmail.com>
2026-08-21 20:26:33 +0200

    Odeslani watchlistu a oprava START.bat

--- START.bat
+++ START.bat
@@ -1,5 +1,2 @@
 @echo off
-echo ==================================================
-echo         SPOUSTIM POKORNY TERMINAL...
-echo ==================================================
-streamlit run 1_Home.py --server.address=127.0.0.1
+streamlit run Dashboard.py --server.address=127.0.0.1
